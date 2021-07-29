81b80e3d20addc2af359e05dd3924c2aa5ff2b70
Plokie <80136737+Plokie@users.noreply.github.com>
2021-07-29 07:43:40 +0100

    added comments + fix max input val

--- blog/blog.js
+++ blog/blog.js
@@ -9,6 +9,10 @@ let inputField = document.getElementById("pageNumInput");
 inputField.setAttribute("max", (numberOfPages-1).toString());
 var found=0;
 var error=false;
+
+
+//This is a work in progress test thing, i want to try to figure out a way it can automatically count
+//the number of posts in the directory, instead of it being manually recorded.
 async function countPosts()
 {
     // let tryNum=1;
@@ -37,68 +41,70 @@ async function countPosts()
     // console.log("found: "+found);
     return 8;
 }
-
 countPosts().then((num)=>{
     postCount=num;
 });
 
+
+
 function loadPosts(pageNum=0)
 {
-    //console.log(numberOfPages);
+    //Reverses the page to load, this is so page 0=Newest and page 0+...x=Oldest
     pageNum = numberOfPages-(pageNum+1);
-    //console.log(pageNum);
 
+    //Resets the collections
     responses=0;
     requests=[];
     titles=[];
-    var postsSidebar = document.getElementById("sidenav");
+    //var postsSidebar = document.getElementById("sidenav");
     
-    requests=[];
-    responses=0;
+    //Counts from 0 to the number of posts that should be displayed this page
+    //(I could probably simplify this but ehh i dont wanna break it - it works)
     for(var i=0; i< Math.min( (pageNum+1)*postsPerPage , postCount ) - (postsPerPage*pageNum); i++)
     {
-        requests[i]=new XMLHttpRequest();
-        //console.log("request for: posts/post"+(i+1)+".html")
-        requests[i].open('GET',"posts/post"+(i+1+(postsPerPage*pageNum))+".html");
-        //console.log(requests);
-        requests[i].responseType='text';
-        requests[i].onload=()=>{
-            responses++;
-            //console.log(responses);
-            //console.log(requests.length);
-            //console.log(requests);
-            if(responses==requests.length)
+        requests[i]=new XMLHttpRequest(); //Makes a new XMLHttpRequest to add to the requests list.
+        requests[i].open('GET',"posts/post"+(i+1+(postsPerPage*pageNum))+".html"); //Gets the corresponding post
+        requests[i].responseType='text'; //Makes sure it reads it as plaintext
+        requests[i].onload=()=>{ //Upon loading
+            responses++; //Increase the recorded number of responses
+            if(responses==requests.length) //If all the responses have been received
             {
-                console.log("done");
-                displayPosts();
+                console.log("done"); //Log done (for debug purposes)
+                displayPosts(); //Call displayPosts, which, get this; Displays the posts.
             }
         }
-        requests[i].send();
+        requests[i].send(); //Sends the XMLHttpRequest
     }
 }
+//Displays the posts that are currently loaded into the "requests" array.
+//Function should only be called when "requests" contains only requests with responses.
 function displayPosts()
 {
-    var postsSidebar = document.getElementById("sidenav");
-    var posts = document.getElementById("blog-posts")
-    posts.innerHTML="";
+    //var postsSidebar = document.getElementById("sidenav");
+    var posts = document.getElementById("blog-posts") //Getss the div where posts go
+    posts.innerHTML=""; //Clears any currently loaded posts
 
+    //Loops through each loaded request in reverse order
+    //This is so that the newest is loaded at the top of the page, and oldest bottom.
     for(var i=requests.length-1; i>=0; i--)
     {
-        //console.log(i);
-        //console.log(requests[i].responseText);
+        //Adds a new div post, with the html content inside it.
         posts.innerHTML+="<div class=\"blog-post\">"+requests[i].responseText+"</div>";
     }
 }
+//Called when the page number input value is changed
 function pageNumChange()
 {
+    //Fetches the page number input box
     let pageNumInput = document.getElementById("pageNumInput");
 
-    if(pageNumInput.value>numberOfPages)
+    //If the inputted number is too large, lower it.
+    if(pageNumInput.value>numberOfPages-1)
     {
-        pageNumInput.value=numberOfPages;
+        pageNumInput.value=numberOfPages-1;
     }
     else
-    {
+    { //Load the page by passing in the page number as a parsed int
         console.log(parseInt(pageNumInput.value));
         loadPosts(parseInt(pageNumInput.value));
     }
