dbbbf51ae2d5d7267be32cdf764b63ed2e8492d6
Plokie <80136737+Plokie@users.noreply.github.com>
2021-12-01 12:45:07 +0000

    Update spoilertag.js

--- sketches/spoilertag.js
+++ sketches/spoilertag.js
@@ -2,4 +2,5 @@ function spoiler(btn){
   console.log(btn.getAttribute("for"));
   var image = document.querySelectorAll('[tag='+btn.getAttribute("for")+']')[0];
   image.style.filter = "blur(0px)";
+  btn.remove();
 }
